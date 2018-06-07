5900c2d0b1fa79f696e7a6b5a8b5c3e1fbe3ce8a
Joelfong1 <Joelfong1@gmail.com>
2018-06-07 18:17:01 -0300

    JF - FA: Login Test and Class
    Adds a unit test for login class.
    
    Co-Authored-By: Fahad ALFaleh <fahad.k.alfaleh@gmail.com>

---
+++ app/src/main/java/com/example/armando/project/logIn.java
@@ -0,0 +1,12 @@
+package com.example.armando.project;
+
+public class logIn {
+
+    public static int validate(String inputUser, String inputPass){
+        //Check user table to see if username exits
+        // if not, return 0
+        //then check if password matches user's password from table
+        //return 1 if valid, 0 if not
+        return 1;
+    }
+}
