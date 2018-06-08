f46d443c8a16382f9a00a98f97e9fe13d006068e
dahnbalan <dahnbalan@dal.ca>
2018-06-08 15:41:13 -0300

    DB + NM: Added Filter class and updated unit tests accordingly

---
+++ app/src/main/java/com/example/armando/project/Filter.java
@@ -0,0 +1,10 @@
+package com.example.armando.project;
+
+public class Filter {
+
+    public boolean filterByClassLevel(int level) {
+        // Querying database to filter class by class level
+        // Updating display
+        return true;
+    }
+}
--- app/src/test/java/com/example/armando/project/FilterUnitTest.java
+++ app/src/test/java/com/example/armando/project/FilterUnitTest.java
@@ -7,20 +7,22 @@ import static org.junit.Assert.assertTrue;
 
 public class FilterUnitTest {
 
+    static Filter filter;
     @Before
     public void setup() {
         //Initialize
+        filter = new Filter();
     }
 
     @Test
     public void filterCourseLevel () {
         //test if it filters courses correctly
-        assertTrue(true);
+        assertTrue(filter.filterByClassLevel(1000));
     }
 
     @Test
     public void filterNoCourseLevel () {
         //test errors when no courses are showing after filter
-        assertTrue(true);
+        assertTrue(filter.filterByClassLevel(2000));
     }
 }
