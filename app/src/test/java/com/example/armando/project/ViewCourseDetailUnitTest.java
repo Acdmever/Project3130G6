13185186eb3ee5ee0d25a88a22a6df148bfb4eb4
dahnbalan <dahnbalan@dal.ca>
2018-06-08 14:37:23 -0300

    DB + NM: Updated Unit tests for viewing course details

---
+++ app/src/test/java/com/example/armando/project/ViewCourseDetailUnitTest.java
@@ -0,0 +1,25 @@
+package com.example.armando.project;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class ViewCourseDetailUnitTest {
+
+    @Before
+    public void setup() {
+        //Initializing course details object
+    }
+
+    @Test
+    public void showCourseDetails () {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showCourseDetailError () {
+        assertTrue(true);
+    }
+}
