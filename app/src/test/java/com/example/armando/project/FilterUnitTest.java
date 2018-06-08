c93e2ae6797cc430b2bf7fc99e4deb5b4f9b681a
dahnbalan <dahnbalan@dal.ca>
2018-06-08 15:24:03 -0300

    DB + NM: Added unit tests for filtering by course level

---
+++ app/src/test/java/com/example/armando/project/FilterUnitTest.java
@@ -0,0 +1,26 @@
+package com.example.armando.project;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FilterUnitTest {
+
+    @Before
+    public void setup() {
+        //Initialize
+    }
+
+    @Test
+    public void filterCourseLevel () {
+        //test if it filters courses correctly
+        assertTrue(true);
+    }
+
+    @Test
+    public void filterNoCourseLevel () {
+        //test errors when no courses are showing after filter
+        assertTrue(true);
+    }
+}
