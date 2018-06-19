c350d675065fb8365ab6f7ff6e82c80bc44d17d9
dahnbalan <dahnbalan@dal.ca>
2018-06-19 00:14:42 -0300

    DB + NM: UI update. Updated View Course Details Activity. Added necessary TextViews in ViewCourseDetail class.

--- app/src/main/java/com/example/armando/project/ViewCourseDetail.java
+++ app/src/main/java/com/example/armando/project/ViewCourseDetail.java
@@ -2,13 +2,31 @@ package com.example.armando.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewCourseDetail extends AppCompatActivity {
+
+    public static TextView txtDepartment;
+    public static TextView txtInstructor;
+    public static TextView txtName;
+    public static TextView txtNumber;
+    public static TextView txtSemester;
+    public static TextView txtYear;
+    public static TextView txtDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_course_detail);
+
+        txtDepartment = findViewById(R.id.Department);
+        txtInstructor = findViewById(R.id.Instructor);
+        txtName = findViewById(R.id.Name);
+        txtNumber = findViewById(R.id.Number);
+        txtSemester = findViewById(R.id.Semester);
+        txtYear = findViewById(R.id.Year);
+        txtDescription = findViewById(R.id.Description);
+
     }
 
     public boolean showCourseDetail(String[] courseDetails) {
